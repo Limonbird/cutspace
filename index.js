618b0e68bf6d3333827b8d88257f8630e8e91460
Yuliya Karkavina <julia-barb@yandex.ru>
2023-03-03 17:43:32 +1200

    Доработан js для формы intro

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // добавляем класс active на форму
 const introForm = document.querySelector('.intro-form');
 const introFormInput = introForm.querySelector('.intro-form__input');
+const introFormButton = introForm.querySelector('.intro-form__button');
+const introElementList = [introFormInput, introFormButton];
 
-introFormInput.addEventListener('focus', () => {
-  introForm.classList.add('intro-form--active');
-})
+introElementList.forEach((item) => {
+  item.addEventListener('focus', () => {
+    introForm.classList.add('intro-form--active');
+  });
+});
 
-introFormInput.addEventListener('blur', () => {
-  introForm.classList.remove('intro-form--active');
-})
+introElementList.forEach((item) => {
+  item.addEventListener('blur', () => {
+    introForm.classList.remove('intro-form--active');
+  });
+});
