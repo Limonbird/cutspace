95362a4ad1a3b0569627c639ec9e6f8fd2bc0954
Yuliya Karkavina <julia-barb@yandex.ru>
2023-04-04 19:08:11 +1200

    Доработано бургер-меню
    Отключен скролл сайта при открытом меню
    При клику по ссылкам меню закрывается
    При резайзе также закрывается
    Для оптимизации ресайза добавлена debounce-функция

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// добавляем класс active на форму
+// добавить класс active на форму
 const introForm = document.querySelector('.intro-form');
 const introFormInput = introForm.querySelector('.intro-form__input');
 const introFormButton = introForm.querySelector('.intro-form__button');
@@ -15,3 +15,53 @@ introElementList.forEach((item) => {
     introForm.classList.remove('intro-form--active');
   });
 });
+// end
+
+// бургер-меню для мобайла: скрывается при клику по ссылке, резайзу до десктопа
+const headerCheckbox = document.querySelector('.header__checkbox');
+const headerLinkList = document.querySelectorAll('.header__link');
+const siteBody = document.querySelector('body');
+
+const closeMenu = () => {
+  headerCheckbox.checked = false;
+  const changeEvent = new Event('change');
+  headerCheckbox.dispatchEvent(changeEvent);
+};
+
+const addMenuClose = () => {
+  headerLinkList.forEach((item) => {
+    item.addEventListener('click', closeMenu);
+  });
+};
+
+const removeMenuClose = () => {
+  headerLinkList.forEach((item) => {
+    item.removeEventListener('click', closeMenu);
+  });
+};
+
+headerCheckbox.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    siteBody.classList.add('body--menu-opened');
+    addMenuClose();
+  } else {
+    siteBody.classList.remove('body--menu-opened');
+    removeMenuClose();
+  }
+});
+
+const debounceForMenu = (func, delay = 250) => {
+  let timeout;
+
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (window.innerWidth >= 992 && headerCheckbox.checked) {
+        func();
+      }
+    }, delay);
+  };
+};
+
+window.addEventListener('resize', debounceForMenu(closeMenu));
+// end
