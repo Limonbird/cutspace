053157b5a3bd0db165b03f4abdbb39039adb766a
Yuliya Karkavina <julia-barb@yandex.ru>
2023-04-14 17:33:01 +1200

    Адаптирован футер
    В mobile блоки с ссылками разворачиваются по клику на title
    При ресайзе до десктопа блоки разворачиваются

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ introElementList.forEach((item) => {
 /*
 бургер-меню для мобайла
 запрет скролла body при открытом меню
-меню открывается при клике на кнопку, закрывается при клике на кнопку, клику по ссылке (это может быть якорь), резайзу окна
+меню открывается при клике на кнопку, закрывается при клике на кнопку, клику по ссылке (это может быть якорь)
 */
 const siteBody = document.querySelector('body');
 const headerLinkList = document.querySelectorAll('.header__link');
@@ -45,19 +45,62 @@ const removeMenuClose = () => {
 menuButton.addEventListener('click', () => {
   siteBody.classList.toggle('body--menu-opened') ? addMenuClose() : removeMenuClose();
 });
+// end
 
-const debounceForMenu = (callback, delay = 250) => {
+// развернуть | свернуть список ссылок в блоках в футере
+const footerUnitAll = document.querySelectorAll('.footer-unit');
+const footerUnitTitleAll = document.querySelectorAll('.footer-unit__title');
+const footerUnitListAll = document.querySelectorAll('.footer-unit__list');
+
+footerUnitTitleAll.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if (window.innerWidth < 576) {
+      footerUnitAll[index].classList.toggle('footer-unit--expanded');
+
+      // развернуть список ссылок на его высоту (по умолчанию в css height: 0)
+      const list = footerUnitListAll[index];
+      if (!list.style.height || list.style.height === '0px') {
+        list.style.height = list.scrollHeight + 'px';
+      } else {
+        list.style.height = '0px';
+      }
+    }
+  });
+});
+
+const removeFooterStyles = () => {
+  footerUnitListAll.forEach((item) => {
+    item.style.removeProperty('height');
+  });
+
+  footerUnitTitleAll.forEach((item) => {
+    item.classList.remove('footer-unit--expanded');
+  });
+};
+// end
+
+// обработка события resize
+// при переходе в десктоп - закрыть бургер меню и убрать инлайн стили height для блоков ссылок в футере
+const debounce = (callback, delay = 250) => {
   let timeout;
 
   return () => {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      if (window.innerWidth >= 992) {
-        callback();
-      }
+      callback();
     }, delay);
   };
 };
 
-window.addEventListener('resize', debounceForMenu(closeMenu));
+const handleResize = () => {
+  if (window.innerWidth >= 992) {
+    closeMenu();
+  }
+
+  if (window.innerWidth >= 576) {
+    removeFooterStyles();
+  }
+};
+
+window.addEventListener('resize', debounce(handleResize));
 // end
